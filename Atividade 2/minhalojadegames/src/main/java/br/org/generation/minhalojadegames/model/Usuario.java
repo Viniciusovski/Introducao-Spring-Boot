11454a104d89f16cf48b5c7c21fce187f5be8e00
unknown <vini98.2012@gmail.com>
2021-10-13 14:16:37 -0300

    implementando testes JUnit em minha loja de games

--- Atividade 2/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/model/Usuario.java
+++ Atividade 2/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/model/Usuario.java
@@ -36,6 +36,17 @@ public class Usuario {
 	@OneToMany(mappedBy="usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Produto> produto;
+	
+	
+
+	public Usuario(long id, String nome, String usuario, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+
+	public Usuario() {  }
 
 	public long getId() {
 		return id;
--- Atividade 2/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/repository/UsuarioRepository.java
+++ Atividade 2/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.org.generation.minhalojadegames.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import br.org.generation.minhalojadegames.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public Optional<Usuario> findByUsuario(String usuario);
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	public Usuario findByNome(String nome);
 }
